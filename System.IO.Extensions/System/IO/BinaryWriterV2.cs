f8a79ce23ba4fa9592059e437b52de59e1da3326
ChaeSeongSik <css5831@outlook.com>
2024-09-23 13:51:51 +0900

    BinaryWriterV2 Addition

---
+++ System.IO.Extensions/System/IO/BinaryWriterV2.cs
@@ -0,0 +1,254 @@
+﻿namespace System.IO
+{
+	using Buffers.Binary;
+	using Reflection;
+	using Text;
+
+	/// <summary>
+	/// Writes the basic format of binary files to a stream according to ByteOrder and supports writing strings in a specific encoding.
+	/// </summary>
+	/// <param name="output"></param>
+	/// <param name="encoding"></param>
+	/// <param name="leaveOpen"></param>
+	/// <param name="order"></param>
+	public sealed class BinaryWriterV2(Stream output, Encoding encoding, bool leaveOpen, ByteOrder order) : BinaryWriter(output, encoding, leaveOpen)
+	{
+		/// <summary>
+		/// Specific streams and ByteOrder
+		/// </summary>
+		/// <param name="output"></param>
+		/// <param name="order"></param>
+		public BinaryWriterV2(Stream output, ByteOrder order = ByteOrder.LittleEndian) : this(output, Encoding.UTF8, order) { }
+
+		/// <summary>
+		/// Specific streams and encodings and ByteOrder
+		/// </summary>
+		/// <param name="output"></param>
+		/// <param name="encoding"></param>
+		/// <param name="order"></param>
+		public BinaryWriterV2(Stream output, Encoding encoding, ByteOrder order = ByteOrder.LittleEndian) : this(output, encoding, false, order) { }
+
+		/// <summary>
+		/// Byte Order
+		/// </summary>
+		public ByteOrder Order => order;
+
+		/// <summary>
+		/// BinaryWriterV2 Override Write Int16
+		/// </summary>
+		/// <param name="value"></param>
+		public override void Write(short value)
+		{
+			Span<byte> buffer = stackalloc byte[sizeof(short)];
+			switch (Order)
+			{
+				case ByteOrder.BigEndian:
+					BinaryPrimitives.WriteInt16BigEndian(buffer, value);
+					break;
+				default:
+					BinaryPrimitives.WriteInt16LittleEndian(buffer, value);
+					break;
+			}
+			OutStream.Write(buffer);
+		}
+
+		/// <summary>
+		/// BinaryWriterV2 Override Write UInt16
+		/// </summary>
+		/// <param name="value"></param>
+		public override void Write(ushort value)
+		{
+			Span<byte> buffer = stackalloc byte[sizeof(ushort)];
+			switch (Order)
+			{
+				case ByteOrder.BigEndian:
+					BinaryPrimitives.WriteUInt16BigEndian(buffer, value);
+					break;
+				default:
+					BinaryPrimitives.WriteUInt16LittleEndian(buffer, value);
+					break;
+			}
+			OutStream.Write(buffer);
+		}
+
+		/// <summary>
+		/// BinaryWriterV2 Override Write Int32
+		/// </summary>
+		/// <param name="value"></param>
+		public override void Write(int value)
+		{
+			Span<byte> buffer = stackalloc byte[sizeof(int)];
+			switch (Order)
+			{
+				case ByteOrder.BigEndian:
+					BinaryPrimitives.WriteInt32BigEndian(buffer, value);
+					break;
+				default:
+					BinaryPrimitives.WriteInt32LittleEndian(buffer, value);
+					break;
+			}
+			OutStream.Write(buffer);
+		}
+
+		/// <summary>
+		/// BinaryWriterV2 Override Write UInt32
+		/// </summary>
+		/// <param name="value"></param>
+		public override void Write(uint value)
+		{
+			Span<byte> buffer = stackalloc byte[sizeof(uint)];
+			switch (Order)
+			{
+				case ByteOrder.BigEndian:
+					BinaryPrimitives.WriteUInt32BigEndian(buffer, value);
+					break;
+				default:
+					BinaryPrimitives.WriteUInt32LittleEndian(buffer, value);
+					break;
+			}
+			OutStream.Write(buffer);
+		}
+
+		/// <summary>
+		/// BinaryWriterV2 Override Write Int64
+		/// </summary>
+		/// <param name="value"></param>
+		public override void Write(long value)
+		{
+			Span<byte> buffer = stackalloc byte[sizeof(long)];
+			switch (Order)
+			{
+				case ByteOrder.BigEndian:
+					BinaryPrimitives.WriteInt64BigEndian(buffer, value);
+					break;
+				default:
+					BinaryPrimitives.WriteInt64LittleEndian(buffer, value);
+					break;
+			}
+			OutStream.Write(buffer);
+		}
+
+		/// <summary>
+		/// BinaryWriterV2 Override Write UInt64
+		/// </summary>
+		/// <param name="value"></param>
+		public override void Write(ulong value)
+		{
+			Span<byte> buffer = stackalloc byte[sizeof(ulong)];
+			switch (Order)
+			{
+				case ByteOrder.BigEndian:
+					BinaryPrimitives.WriteUInt64BigEndian(buffer, value);
+					break;
+				default:
+					BinaryPrimitives.WriteUInt64LittleEndian(buffer, value);
+					break;
+			}
+			OutStream.Write(buffer);
+		}
+
+		/// <summary>
+		/// BinaryWriterV2 Override Write Half
+		/// </summary>
+		/// <param name="value"></param>
+		public override void Write(Half value)
+		{
+			Span<byte> buffer = stackalloc byte[sizeof(ushort)];
+			switch (Order)
+			{
+				case ByteOrder.BigEndian:
+					BinaryPrimitives.WriteHalfBigEndian(buffer, value);
+					break;
+				default:
+					BinaryPrimitives.WriteHalfLittleEndian(buffer, value);
+					break;
+			}
+			OutStream.Write(buffer);
+		}
+
+		/// <summary>
+		/// BinaryWriterV2 Override Write Single
+		/// </summary>
+		/// <param name="value"></param>
+		public override void Write(float value)
+		{
+			Span<byte> buffer = stackalloc byte[sizeof(float)];
+			switch (Order)
+			{
+				case ByteOrder.BigEndian:
+					BinaryPrimitives.WriteSingleBigEndian(buffer, value);
+					break;
+				default:
+					BinaryPrimitives.WriteSingleLittleEndian(buffer, value);
+					break;
+			}
+			OutStream.Write(buffer);
+		}
+
+		/// <summary>
+		/// BinaryWriterV2 Override Write Double
+		/// </summary>
+		/// <param name="value"></param>
+		public override void Write(double value)
+		{
+			Span<byte> buffer = stackalloc byte[sizeof(double)];
+			switch (Order)
+			{
+				case ByteOrder.BigEndian:
+					BinaryPrimitives.WriteDoubleBigEndian(buffer, value);
+					break;
+				default:
+					BinaryPrimitives.WriteDoubleLittleEndian(buffer, value);
+					break;
+			}
+			OutStream.Write(buffer);
+		}
+
+		/// <summary>
+		/// BinaryWriterV2 Override Write Decimal
+		/// </summary>
+		/// <param name="value"></param>
+		public override void Write(decimal value)
+		{
+			Type decimalType = typeof(decimal);
+			PropertyInfo? lowProperty = decimalType.GetProperty("Low", BindingFlags.NonPublic | BindingFlags.Instance);
+			ArgumentNullException.ThrowIfNull(lowProperty);
+			uint? Low = (uint?)lowProperty.GetValue(value);
+
+			PropertyInfo? midProperty = decimalType.GetProperty("Mid", BindingFlags.NonPublic | BindingFlags.Instance);
+			ArgumentNullException.ThrowIfNull(midProperty);
+			uint? Mid = (uint?)midProperty.GetValue(value);
+
+			PropertyInfo? highProperty = decimalType.GetProperty("High", BindingFlags.NonPublic | BindingFlags.Instance);
+			ArgumentNullException.ThrowIfNull(highProperty);
+			uint? High = (uint?)highProperty.GetValue(value);
+
+			FieldInfo? _flagsField = decimalType.GetField("_flags", BindingFlags.NonPublic | BindingFlags.Instance);
+			ArgumentNullException.ThrowIfNull(_flagsField);
+			int? _flags = (int?)_flagsField.GetValue(value);
+
+			ArgumentNullException.ThrowIfNull(Low);
+			ArgumentNullException.ThrowIfNull(Mid);
+			ArgumentNullException.ThrowIfNull(High);
+			ArgumentNullException.ThrowIfNull(_flags);
+
+			Span<byte> buffer = stackalloc byte[sizeof(decimal)];
+			switch (Order)
+			{
+				case ByteOrder.BigEndian:
+					BinaryPrimitives.WriteInt32BigEndian(buffer, _flags.Value);
+					BinaryPrimitives.WriteInt32BigEndian(buffer[4..], (int)High.Value);
+					BinaryPrimitives.WriteInt32BigEndian(buffer[8..], (int)Mid.Value);
+					BinaryPrimitives.WriteInt32BigEndian(buffer[12..], (int)Low.Value);
+					break;
+				default:
+					BinaryPrimitives.WriteInt32LittleEndian(buffer, (int)Low.Value);
+					BinaryPrimitives.WriteInt32LittleEndian(buffer[4..], (int)Mid.Value);
+					BinaryPrimitives.WriteInt32LittleEndian(buffer[8..], (int)High.Value);
+					BinaryPrimitives.WriteInt32LittleEndian(buffer[12..], _flags.Value);
+					break;
+			}
+			OutStream.Write(buffer);
+		}
+	}
+}
---
+++ System.IO.ExtensionsTests/System/IO/BinaryWriterV2Tests.cs
@@ -0,0 +1,190 @@
+﻿namespace System.IO.Tests
+{
+	[TestClass]
+	public class BinaryWriterV2Tests
+	{
+		[TestMethod]
+		public void WriteTest()
+		{
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory);
+				using BinaryWriter writer = new BinaryWriterV2(memory);
+				writer.Write(short.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(short.MaxValue, reader.ReadInt16());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory, ByteOrder.BigEndian);
+				using BinaryWriter writer = new BinaryWriterV2(memory, ByteOrder.BigEndian);
+				writer.Write(short.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(short.MaxValue, reader.ReadInt16());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory);
+				using BinaryWriter writer = new BinaryWriterV2(memory);
+				writer.Write(ushort.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(ushort.MaxValue, reader.ReadUInt16());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory, ByteOrder.BigEndian);
+				using BinaryWriter writer = new BinaryWriterV2(memory, ByteOrder.BigEndian);
+				writer.Write(ushort.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(ushort.MaxValue, reader.ReadUInt16());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory);
+				using BinaryWriter writer = new BinaryWriterV2(memory);
+				writer.Write(int.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(int.MaxValue, reader.ReadInt32());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory, ByteOrder.BigEndian);
+				using BinaryWriter writer = new BinaryWriterV2(memory, ByteOrder.BigEndian);
+				writer.Write(int.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(int.MaxValue, reader.ReadInt32());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory);
+				using BinaryWriter writer = new BinaryWriterV2(memory);
+				writer.Write(uint.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(uint.MaxValue, reader.ReadUInt32());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory, ByteOrder.BigEndian);
+				using BinaryWriter writer = new BinaryWriterV2(memory, ByteOrder.BigEndian);
+				writer.Write(uint.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(uint.MaxValue, reader.ReadUInt32());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory);
+				using BinaryWriter writer = new BinaryWriterV2(memory);
+				writer.Write(long.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(long.MaxValue, reader.ReadInt64());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory, ByteOrder.BigEndian);
+				using BinaryWriter writer = new BinaryWriterV2(memory, ByteOrder.BigEndian);
+				writer.Write(long.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(long.MaxValue, reader.ReadInt64());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory);
+				using BinaryWriter writer = new BinaryWriterV2(memory);
+				writer.Write(ulong.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(ulong.MaxValue, reader.ReadUInt64());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory, ByteOrder.BigEndian);
+				using BinaryWriter writer = new BinaryWriterV2(memory, ByteOrder.BigEndian);
+				writer.Write(ulong.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(ulong.MaxValue, reader.ReadUInt64());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory);
+				using BinaryWriter writer = new BinaryWriterV2(memory);
+				writer.Write(Half.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(Half.MaxValue, reader.ReadHalf());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory, ByteOrder.BigEndian);
+				using BinaryWriter writer = new BinaryWriterV2(memory, ByteOrder.BigEndian);
+				writer.Write(Half.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(Half.MaxValue, reader.ReadHalf());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory);
+				using BinaryWriter writer = new BinaryWriterV2(memory);
+				writer.Write(float.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(float.MaxValue, reader.ReadSingle());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory, ByteOrder.BigEndian);
+				using BinaryWriter writer = new BinaryWriterV2(memory, ByteOrder.BigEndian);
+				writer.Write(float.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(float.MaxValue, reader.ReadSingle());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory);
+				using BinaryWriter writer = new BinaryWriterV2(memory);
+				writer.Write(double.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(double.MaxValue, reader.ReadDouble());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory, ByteOrder.BigEndian);
+				using BinaryWriter writer = new BinaryWriterV2(memory, ByteOrder.BigEndian);
+				writer.Write(double.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(double.MaxValue, reader.ReadDouble());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory);
+				using BinaryWriter writer = new BinaryWriterV2(memory);
+				writer.Write(decimal.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(decimal.MaxValue, reader.ReadDecimal());
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory, ByteOrder.BigEndian);
+				using BinaryWriter writer = new BinaryWriterV2(memory, ByteOrder.BigEndian);
+				writer.Write(decimal.MaxValue);
+				memory.Position = 0;
+				Assert.AreEqual(decimal.MaxValue, reader.ReadDecimal());
+			}
+		}
+	}
+}
