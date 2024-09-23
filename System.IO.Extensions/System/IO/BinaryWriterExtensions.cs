5b62fcf78ece14f5d4f7d5e1726fec12a1a91df3
ChaeSeongSik <css5831@outlook.com>
2024-09-23 14:00:10 +0900

    BinaryWriter Write IPAddress Extensions Addition

---
+++ System.IO.Extensions/System/IO/BinaryWriterExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace System.IO
+{
+	using Net;
+
+	/// <summary>
+	/// System.IO.BinaryWriter Extensions
+	/// </summary>
+	public static class BinaryWriterExtensions
+	{
+		/// <summary>
+		/// System.IO.BinaryWriter Write System.Net.IPAddress
+		/// </summary>
+		/// <param name="writer"></param>
+		/// <param name="address"></param>
+		public static void Write(this BinaryWriter writer, IPAddress address)
+		{
+			ByteOrder order = ByteOrder.BigEndian;
+			if (writer is BinaryWriterV2 v)
+				order = v.Order;
+			byte[] buf = address.GetAddressBytes();
+			if (order == ByteOrder.LittleEndian)
+				Array.Reverse(buf);
+			writer.Write(buf);
+		}
+	}
+}
---
+++ System.IO.ExtensionsTests/System/IO/BinaryWriterExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿namespace System.IO.Tests
+{
+	using Net;
+	using Net.Sockets;
+
+	[TestClass]
+	public class BinaryWriterExtensionsTests
+	{
+		[TestMethod]
+		public void WriteTest()
+		{
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReader(memory);
+				using BinaryWriter writer = new BinaryWriter(memory);
+				writer.Write(IPAddress.Loopback);
+				memory.Position = 0;
+				IPAddress address = reader.ReadIPAddress(AddressFamily.InterNetwork);
+				Assert.AreEqual(IPAddress.Loopback, address);
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReader(memory);
+				using BinaryWriter writer = new BinaryWriter(memory);
+				writer.Write(IPAddress.IPv6Loopback);
+				memory.Position = 0;
+				IPAddress address = reader.ReadIPAddress(AddressFamily.InterNetworkV6);
+				Assert.AreEqual(IPAddress.IPv6Loopback, address);
+			}
+
+			{
+				using MemoryStream memory = new MemoryStream();
+				using BinaryReader reader = new BinaryReaderV2(memory);
+				using BinaryWriter writer = new BinaryWriterV2(memory);
+				writer.Write(IPAddress.Loopback);
+				memory.Position = 0;
+				IPAddress address = reader.ReadIPAddress(AddressFamily.InterNetwork);
+				Assert.AreEqual(IPAddress.Loopback, address);
+			}
+		}
+	}
+}
