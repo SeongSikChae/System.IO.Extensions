db60cdc56e47e6bbf3e9fa54a8dc57d89b16b533
ChaeSeongSik <css5831@outlook.com>
2024-09-29 11:59:26 +0900

    ReadUTF8, WriteUTF8 추가

--- System.IO.Extensions/System/IO/BinaryReaderExtensions.cs
+++ System.IO.Extensions/System/IO/BinaryReaderExtensions.cs
@@ -2,6 +2,7 @@
 {
 	using Net;
 	using Net.Sockets;
+	using System.Text;
 
 	/// <summary>
 	/// System.IO.BinaryReader Extensions
@@ -29,6 +30,21 @@
 			if (order == ByteOrder.LittleEndian)
 				Array.Reverse(buf);
 			return new IPAddress(buf);
+		}
+
+		/// <summary>
+		/// UTF-8 바이트 길이 + UTF-8 디코딩 바이너리 형태로 기록된 데이터를 읽습니다.
+		/// </summary>
+		/// <param name="reader"></param>
+		/// <returns></returns>
+		public static string ReadUTF8(this BinaryReader reader)
+		{
+			int length = reader.ReadInt32();
+			if (length < 0)
+				length = 0;
+			byte[] buf = new byte[length];
+			byte[] valueBlock = reader.ReadBytes(length);
+			return Encoding.UTF8.GetString(valueBlock);
 		}
 	}
 }
--- System.IO.Extensions/System/IO/BinaryWriterExtensions.cs
+++ System.IO.Extensions/System/IO/BinaryWriterExtensions.cs
@@ -1,6 +1,7 @@
 ﻿namespace System.IO
 {
 	using Net;
+	using Text;
 
 	/// <summary>
 	/// System.IO.BinaryWriter Extensions
@@ -21,6 +22,18 @@
 			if (order == ByteOrder.LittleEndian)
 				Array.Reverse(buf);
 			writer.Write(buf);
+		}
+
+		/// <summary>
+		/// 문자열을 BinaryWrtier에 문자열의 UTF-8 바이트 길이 + UTF-8 디코딩 바이너리 형태로 기록합니다.
+		/// </summary>
+		/// <param name="writer"></param>
+		/// <param name="value"></param>
+		public static void WriteUTF8(this BinaryWriter writer, string value)
+		{
+			byte[] valueBlock = Encoding.UTF8.GetBytes(value);
+			writer.Write(valueBlock.Length);
+			writer.Write(valueBlock);
 		}
 	}
 }
